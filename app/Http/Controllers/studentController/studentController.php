fc9859a4dd7d77c933728d0b271165f552add770
Yousof Kortam <115800563+yousofkortam@users.noreply.github.com>
2023-05-10 04:38:49 +0300

    add studentContoller & professorController

---
+++ app/Http/Controllers/studentController/studentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\studentController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class studentController extends Controller
+{
+    //
+}
