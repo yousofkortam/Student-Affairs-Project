44709456ad77bb411c480b595fb03bf2250e3af2
Yousof Kortam <115800563+yousofkortam@users.noreply.github.com>
2023-05-19 00:12:50 +0300

    Delete unused files

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -152,8 +152,6 @@
     </script>
 
     <script src="/vendors/jquery/dist/jquery.min.js"></script>
-    <script src="/vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="/assets/js/main.js"></script>
 
 
@@ -161,9 +159,6 @@
     <script src="/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
     <script src="/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
     <script src="/vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-    <script src="/vendors/jszip/dist/jszip.min.js"></script>
-    <script src="/vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="/vendors/pdfmake/build/vfs_fonts.js"></script>
     <script src="/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
     <script src="/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
     <script src="/vendors/datatables.net-buttons/js/buttons.colVis.min.js"></script>
