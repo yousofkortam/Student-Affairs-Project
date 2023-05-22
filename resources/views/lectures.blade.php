f64b2a5113a2a8a811c3d3a838dafd23914e6e44
DEVenici <akdev0101@gmail.com>
2023-05-22 23:49:17 +0300

    add download button

--- resources/views/lectures.blade.php
+++ resources/views/lectures.blade.php
@@ -1,31 +1,38 @@
 @extends('layouts.studentLayout')
 
 @section('page title')
-    @if (count($lectures) > 0)
-        {{ $lectures[0]->course->course_name }} |
-    @endif
-    Lectures
+@if (count($lectures) > 0)
+{{ $lectures[0]->course->course_name }} |
+@endif
+Lectures
 @endsection
 
 @section('styles')
-    <link rel="stylesheet" href="/lec.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+<link rel="stylesheet" href="/lec.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 @endsection
 
 @section('content')
-    <div class="container">
-        @foreach ($lectures as $lecture)
-            <a href="/lectures/{{ $lecture->file_path }}" class="card-link" target="_blank">
-                <div class="card">
-                    <div class="card-icon">
-                        <i class="fas fa-file-pdf"></i>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $lecture->title }}</h5>
-                        <p class="card-text">{{ $lecture->description }}</p>
-                    </div>
+<div class="container">
+    @foreach ($lectures as $lecture)
+    <a href="/lectures/{{ $lecture->file_path }}" class="card-link" target="_blank">
+        <div class="card">
+            <div class="card-icon">
+                <i class="fas fa-file-pdf"></i>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">{{ $lecture->title }}</h5>
+                <div style="display: flex; justify-content:space-between;">
+                    <p class="card-text">{{ $lecture->description }}</p>
+                    <button type="button" class="btn btn-primary">
+                        <div class="download-icon">
+                            <i class="fas fa-download"></i>
+                        </div>
+                    </button>
                 </div>
-            </a>
-        @endforeach
-    </div>
+            </div>
+        </div>
+    </a>
+    @endforeach
+</div>
 @endsection
